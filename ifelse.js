c4da89a00c811311ed5567499ab70a7028faac51
AkibRiad <akibriad652@gmail.com>
2024-08-30 19:32:48 +0600

    hudai

---
+++ ifelse.js
@@ -0,0 +1,13 @@
+// if(3 < 5){
+//     console.log('I have a Smaller Value');
+    
+// }
+
+
+var biriyaniPrice = 500;
+if(biriyaniPrice < 300){
+    console.log('Then I will not Buy Biriyaani');
+}
+else{
+    console.log('Then I will not Buy Biriyaani');
+}
---
+++ practice.js
@@ -0,0 +1,5 @@
+var myName = 10;
+var myEx =20;
+
+var myLife = myName + myEx;
+console.log(myLife + 10);
