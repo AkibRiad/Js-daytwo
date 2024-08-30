0c86dc5596bb97fc26b601778188db07cfec9e2e
AkibRiad <akibriad652@gmail.com>
2024-08-30 19:55:32 +0600

    hudaiiiii

---
+++ multicondition.js
@@ -0,0 +1,11 @@
+const salary = 25000;
+const isBCS = true;
+const height = 65;
+
+
+if( salary > 20000 || height > 68){
+    console.log('Khub vlo');
+}
+else{
+    console.log('onno kawke khujte hbe');
+}
